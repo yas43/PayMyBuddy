a46135eeb0b92cebb959568dfd867d52f6a88e38
yas43 <ykeshtdar@yahoo.com>
2024-07-18 15:26:34 +0200

    signUp/logIn pages ui improved

---
+++ src/main/java/com/ykeshtdar/demoP6/model/Fee.java
@@ -0,0 +1,12 @@
+package com.ykeshtdar.demoP6.model;
+
+public enum Fee {
+//     currentFee =(0.005);
+//     private double fee;
+//     Fee(float fee){
+//         this.fee=fee;
+//     }
+//     public float getFee(){
+//         return fee;
+//     }
+}
