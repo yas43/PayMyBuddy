39edaa7cf70c44d398483bf987f043efebd9dd6f
yas43 <ykeshtdar@yahoo.com>
2024-08-08 09:39:45 +0200

    correcting page's name and complete sql script

--- src/main/resources/database/create.sql
+++ src/main/resources/database/create.sql
@@ -4,27 +4,27 @@ CREATE DATABASE dev2payMyBuddy;
 CREATE TABLE User (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255),
-    email VARCHAR(255),
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255),
     role VARCHAR(255),
-    balance FLOAT
+    balance FLOAT NOT NULL
 );
 
 CREATE TABLE friend (
     user_id INT,
     friend_id INT,
     PRIMARY KEY (user_id, friend_id),
-    FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (friend_id) REFERENCES User(id)
+    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (friend_id) REFERENCES User(id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 CREATE TABLE Transaction (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    sender_id INT,
-    receiver_id INT,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
     description VARCHAR(255),
-    amount FLOAT,
+    amount FLOAT NOT NULL,
     fee FLOAT,
     date DATETIME,
-    FOREIGN KEY (sender_id) REFERENCES User(id),
-    FOREIGN KEY (receiver_id) REFERENCES User(id)
+    FOREIGN KEY (sender_id) REFERENCES User(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+    FOREIGN KEY (receiver_id) REFERENCES User(id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
